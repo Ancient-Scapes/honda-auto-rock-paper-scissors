571809d4d734314c1fb14d822af17f2c1c603110
SugarShootingStar <ryu.8224@gmail.com>
2019-07-09 14:39:50 +0900

    fix: headless:falseをやめる

--- index-phase2.js
+++ index-phase2.js
@@ -2,7 +2,9 @@ const puppeteer = require('puppeteer');
 require('dotenv').config();
 
 async function main() {
-  const browser = await puppeteer.launch({headless: false});
+  // デバッグ用
+  // const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   try {
